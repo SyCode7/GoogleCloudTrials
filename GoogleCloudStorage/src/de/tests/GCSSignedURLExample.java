cded9df92e6638180c3a16de58702cc6af148e41
SyCode7 <ktorkura@yahoo.com>
2015-02-13 17:03:54 +0100

    more progress

--- GoogleCloudStorage/src/de/tests/GCSSignedURLExample.java
+++ GoogleCloudStorage/src/de/tests/GCSSignedURLExample.java
@@ -1,18 +1,25 @@
 package de.tests;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.security.KeyStore;
 import java.security.PrivateKey;
+import java.security.Signature;
+
+import org.apache.commons.codec.binary.Base64;
+
 
 public class GCSSignedURLExample {
 	
 	final String SERVICE_ACCOUNT_EMAIL = "139888340867-32r1p9d4hm7gnpfud35uqplbr9gkbvsj@developer.gserviceaccount.com";
-	final String SERVICE_ACCOUNT_PKCS12_FILE_PATH = "C:\\Users\\Kennedy.Torkura\\googleIDTestStorage-6c02d3ae0d7c";
+	final String SERVICE_ACCOUNT_PKCS12_FILE_PATH = "C:\\googleID\\TestStorage-5658073866f9c";
 	final long expiration = System.currentTimeMillis()/1000 + 60;
 	
 	final String BUCKET_NAME = "starting";
@@ -70,29 +77,42 @@ public class GCSSignedURLExample {
 		}
 	}
 
-	private void renderResponse(InputStream inputStream) {
+	private void renderResponse(InputStream is) throws Exception {
 			BufferedReader in = new BufferedReader(new InputStreamReader(is));
-				
-				@Override
-				public int read() throws IOException {
-					// TODO Auto-generated method stub
-					return 0;
-				}
-			})
-			
+			String inputLine;
+			while ((inputLine = in.readLine())!= null)
+					System.out.println(inputLine);
+					in.close();
+	}
+	private String getSigningURL(String verb) throws Exception {
+		String url_signture = this.signString(verb + " \n\n\n"  + expiration +  "\n" + "/" + BUCKET_NAME + OBJECT_NAME );
+		String signed_url = "https://storage.googleapis.com/" + BUCKET_NAME + "/" +  OBJECT_NAME + "?GoogleAccessId=" +
+		SERVICE_ACCOUNT_EMAIL +  "?&Expires=" + expiration + "&Signature=" + URLEncoder.encode(url_signture, "UTF-8");  
+		return signed_url;
 	}
 
-	private String getSigningURL(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	private PrivateKey loadKeyFromPkcs12(
-			String sERVICE_ACCOUNT_PKCS12_FILE_PATH2, char[] cs) {
-		// TODO Auto-generated method stub
-		return null;
+	private PrivateKey loadKeyFromPkcs12( String filename , char[] password) throws Exception {
+		
+		FileInputStream fis = new FileInputStream(filename);
+		KeyStore ks = KeyStore.getInstance("PKCS12");
+		ks.load(fis,password);
+		
+		return (PrivateKey) ks.getKey("privateKey", password);
 	}
 	
+	private String signString(String stringToString ) throws Exception {
+		if (key == null) 
+			throw new Exception("Private Key Not initialised" );
+			Signature signer = Signature.getInstance("SHA256withRSA");
+			signer.initSign(key);
+			signer.update(stringToString.getBytes("UTF-8"));
+			byte [] rawSignature = signer.sign();
+			return new String(Base64.encodeBase64(rawSignature, false), "UTF-8");
+					
+		}
+		
+	}
 	
 
-}
+
